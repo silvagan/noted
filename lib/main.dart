8fce6d381f1c7a5fa0c06f8cfde2f151ddde549b
silva <n.silvagan@gmail.com>
2024-11-19 14:00:41 +0200

    tutorial(main): 9/9 done

--- lib/main.dart
+++ lib/main.dart
@@ -27,39 +27,152 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
- void getNext(){
-  current = WordPair.random();
-  notifyListeners();
+    var current = WordPair.random();
+  void getNext(){
+    current = WordPair.random();
+    notifyListeners();
  }
+
+  var favorites = <WordPair>[];
+  void toggleFavorite(){
+    if (favorites.contains(current)){
+      favorites.remove(current);
+    }else{
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex){
+      case 0:
+        page = GeneratorPage();
+      case 1:
+        page = FavoritesPage();
+      default:
+       throw UnimplementedError('no widget for $selectedIndex');
+    }
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(child: NavigationRail(
+                extended: constraints.maxWidth >= 600,
+                destinations:[
+                  NavigationRailDestination(icon: Icon(Icons.home), label: Text('home')),
+                  NavigationRailDestination(icon: Icon(Icons.favorite), label: Text('Favorites')),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value){
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
+              )),
+              Expanded(child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
+              )),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Text('A random AWESOME idea:'),
-            BigCard(pair: pair),
-            SizedBox(height:10),
-            ElevatedButton(
-              onPressed: (){
-                appState.getNext();
-              },
-              child:Text('Next'),
-            ),
-          ],
-        ),
+
+    IconData icon;
+    if(appState.favorites.contains(pair)){
+      icon = Icons.favorite;
+    } else{
+      icon = Icons.favorite_border;
+    }
+    return Center(child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      BigCard(pair: pair),
+      SizedBox(height:10),
+      Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          ElevatedButton.icon(onPressed: (){
+            appState.toggleFavorite();
+            }, 
+            icon: Icon(icon),
+            label:Text('Like')),
+          SizedBox(width:15,),
+          ElevatedButton(
+            onPressed: (){
+              appState.getNext();
+            },
+            child: Text('Next'),
+          ),
+        ],
       ),
-    );
+    ],),);
   }
 }
+
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty){
+      return Center(child:Text('no favorites yet'));
+    }
+
+    return ListView(
+      children: [
+        Padding(
+        padding: const EdgeInsets.all(20),
+        child:Text('You have ' '${appState.favorites.length} favorites'),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            leading:Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          )
+      ],
+      );
+  }
+}
+
+abstract class ListItem{
+  Widget buildWord(BuildContext context);
+}
+
+class FavoriteItem implements ListItem {
+  final WordPair pair;
+  FavoriteItem(this.pair);
+  @override buildWord(BuildContext context) => Text(pair.asString);
+}
+
+
+
+
+
 
 class BigCard extends StatelessWidget {
   const BigCard({
